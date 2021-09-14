a849eb738484006cc5d97f1cb3a87dd049c88483
Yahia Labib <85199984+yahialabeeb@users.noreply.github.com>
2021-09-14 18:54:01 +0300

    Update getBooks.js

--- modules/getBooks.js
+++ modules/getBooks.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.mongolink)
+mongoose.connect(mongolink)
 // 'mongodb://localhost:27017/books1'
 const bookModel = require('./bookModel.js')
 
