c1a7be0dc0df639516ac176f67b74402ca2e108a
osama900 <osamaos2233@gmail.com>
2021-09-15 15:20:26 +0300

    ddddd

--- modules/getBooks.js
+++ modules/getBooks.js
@@ -1,27 +1,20 @@
-'use strict'
+"use strict";
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
+mongoose.connect(process.env.mongolink);
 
-mongoose.connect(process.env.mongolink)
+const bookModel = require("./bookModel.js");
 
-
-const bookModel = require('./bookModel.js')
-
-
-
-function getBooks(req,res) {
-    
-    let email1 = req.query.email;
-    bookModel.find({email:email1},function(err,ownerData){
-        if(err) {
-            console.log('error in getting the data')
-            res.send("error")
-        } else {
-            console.log(ownerData);
-            res.send(ownerData)
-        }
-    })
+function getBooks(req, res) {
+  bookModel.find({ email: req.query.email }, function (err, ownerData) {
+    if (err) {
+      console.log("error in getting the data");
+      res.send("error");
+    } else {
+      console.log(ownerData);
+      res.send(ownerData);
+    }
+  });
 }
 module.exports = getBooks;
-
--- server.js
+++ server.js
@@ -5,50 +5,45 @@ const cors = require("cors");
 
 require("dotenv").config();
 const getBooks = require("./modules/getBooks.js");
-const bookModel = require('./modules/bookModel.js')
+const bookModel = require("./modules/bookModel.js");
 
 const PORT = process.env.PORT;
 const server = express();
 server.use(express.json());
 server.use(cors());
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.mongolink)
+mongoose.connect(process.env.mongolink);
 //---------------------------------------------------------------------
 
 server.get("/books", getBooks);
 server.delete("/deletebook/:bookId", deleteBookHandler);
-server.post('/addbook', addBookHandler);
+server.post("/addbook", addBookHandler);
 
 server.get("*", (request, response) => {
   response.status(404).send("not found");
 });
 
-
 async function addBookHandler(req, res) {
-
   console.log(req.body);
- 
-  let {title,description,email,status} = req.body;
 
-  await bookModel.create({title,description,email,status})
+  let { title, description, email, status } = req.body;
+
+  await bookModel.create({ title, description, email, status });
 
   bookModel.find({ email }, function (err, ownerData) {
-      if (err) {
-          console.log('error in getting the data')
-      } else {
-          console.log(ownerData);
-          res.send(ownerData)
-      }
-  })
-
-
-
+    if (err) {
+      console.log("error in getting the data");
+    } else {
+      console.log(ownerData);
+      res.send(ownerData);
+    }
+  });
 }
 
 function deleteBookHandler(request, response) {
-  console.log('inside the deleteHandler')
+  console.log("inside the deleteHandler");
   let email1 = request.query.email;
   let bookID = request.params.bookId;
   bookModel.remove({ _id: bookID }, (error, bookData) => {
@@ -56,17 +51,17 @@ function deleteBookHandler(request, response) {
       console.log("error in deleting the data");
     } else {
       console.log(bookData);
-      bookModel.find({ email: email1 }, (err, ownerData)=> {
+      bookModel.find({ email: email1 }, (err, ownerData) => {
         if (err) {
           // console.log('error in getting the data')
-          response.send("error")
+          response.send("error");
         } else {
           // console.log(ownerData);
-          response.send(ownerData)
+          response.send(ownerData);
         }
-      })
+      });
     }
-  })
+  });
 }
 
 server.listen(PORT, () => {
